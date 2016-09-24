f3b8003f4962763b30b04cbae4938545540b3b1b
paarnio <p.aarnio@gmail.com>
2016-09-24 13:19:28 +0300

    initial commit

---
+++ GitHello/src/siima/gittest/HelloEGit.java
@@ -0,0 +1,16 @@
+/*
+ * Ks. https://www.youtube.com/watch?v=cdsMIX9gB94
+ * Video kohta 8:10
+ * RMC/team/share_project
+ */
+
+package siima.gittest;
+
+public class HelloEGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Eclipse Neon Git");
+
+	}
+
+}
