836e20b515fbc7f37ae4a98ed90e90e7c57a4077
paarnio <p.aarnio@gmail.com>
2016-09-24 13:34:05 +0300

    added comment links to good eclipse git tutorial videos

--- GitHello/src/siima/gittest/HelloEGit.java
+++ GitHello/src/siima/gittest/HelloEGit.java
@@ -1,7 +1,15 @@
-/*
+/* HelloEGit.java
  * Ks. https://www.youtube.com/watch?v=cdsMIX9gB94
  * Video kohta 8:10
  * RMC/team/share_project
+ * 
+ * GOOD Tutorial Videos:
+ * Git tutorial 1: creating github repo and sharing project in Eclipse 4.5.1 
+ * https://www.youtube.com/watch?v=cdsMIX9gB94
+ * Git tutorial 2: Committing, pushing, pulling and resolving conflicts with git and Eclipse 4.5.1 
+ * https://www.youtube.com/watch?v=M88sKbRDR8Y#t=2.26777
+ * 
+ * 
  */
 
 package siima.gittest;
